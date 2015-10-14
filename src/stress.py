ae3898fd4d1fa7d2d814c341dabdb9a1d53f67a4
Hanifi Gunes <hgunes@maprtech.com>
2015-10-14 14:19:40 -0700

    adding options

--- src/stress.py
+++ src/stress.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 __author__ = 'hgunes'
+import argparse
 import logging
 import sys
 import concurrent
@@ -7,9 +8,10 @@ import drill
 
 logger = logging.getLogger(__name__)
 
-def run(args):
+def run(opts):
+  logger.info('running with options %s', opts)
   bit = drill.Drillbit()
-  pool = concurrent.ThreadPool(10)
+  pool = concurrent.ThreadPool(opts.concurrency)
 
   def query(i, stmt):
     logger.info('REQ %s', i)
@@ -19,13 +21,22 @@ def run(args):
   stmt = 'select * from cp.`employee.json` limit 1'
   pool.start()
 
-  for i in xrange(100):
-    pool.execute(query, i, stmt)
+  cur, total = 0, opts.requests
+  while True:
+    if not (total == 0 or cur < total): break
+    cur += 1
+    pool.execute(query, cur, stmt)
 
   pool.stop()
   pool.join()
 
+def parseOptions(args):
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-c', action='store', dest='concurrency', default=10, type=int, required=0)
+  parser.add_argument('-r', action='store', dest='requests', default=100, type=int, required=0, help='total # of requests. 0 for infinity')
+  return parser.parse_args(args)
 
 if __name__ == '__main__':
   logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-  run(sys.argv)
+  opts = parseOptions(sys.argv[1:])
+  run(opts)
